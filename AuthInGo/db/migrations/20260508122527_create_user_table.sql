5400a87c00b40f1616b164fb7cee574ce90a0c14
saisathwik22 <reddysaisathwik022@gmail.com>
2026-05-08 18:03:00 +0530

    Added and tested out goose migrations

---
+++ AuthInGo/db/migrations/20260508122527_create_user_table.sql
@@ -0,0 +1,13 @@
+-- +goose Up
+
+CREATE TABLE users (
+	id SERIAL PRIMARY KEY,
+    username VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+-- +goose Down
+DROP TABLE users;
